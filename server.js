62aba0837468c570bef5610482d650666f278275
Anthony Cantu <anthonycantu@Anthonys-MacBook-Air-98.local>
2026-04-27 21:57:17 -0500

    Add Express server for Railway deployment

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'booking.html')));
+app.get('/thankyou', (req, res) => res.sendFile(path.join(__dirname, 'thankyou.html')));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
