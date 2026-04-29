692d05853accd8330e4663b9684dc1d82bd6e805
Anthony Cantu <anthonycantu@Anthonys-MacBook-Air-98.local>
2026-04-29 11:57:07 -0500

    Add disqualified page and route

--- server.js
+++ server.js
@@ -8,5 +8,6 @@ app.use(express.static(path.join(__dirname)));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 app.get('/booking', (req, res) => res.sendFile(path.join(__dirname, 'booking.html')));
 app.get('/thankyou', (req, res) => res.sendFile(path.join(__dirname, 'thankyou.html')));
+app.get('/disqualified', (req, res) => res.sendFile(path.join(__dirname, 'disqualified.html')));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
